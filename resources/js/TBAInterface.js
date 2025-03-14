986ed86c3ee4e27191619242772f1d7b14d0006c
rubymarie5608 <rubymarie5608@gmail.com>
2025-03-14 18:42:30 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "2i0WgugewKzT4scO1gVtnT0q0NIjRj5t992tPIMftgxhbVuZ5nAeJDC74n2PfbYz";
 /**
  * Get list of teams in event
  *
