7577a5fb8a80626c45c9b5390d60d14a94e76274
rubymarie5608 <rubymarie5608@gmail.com>
2025-03-13 20:12:13 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025txpla",
+      "defaultValue": "2025txfw",
       "required": "true"
     },
     { "name": "Match Level",
